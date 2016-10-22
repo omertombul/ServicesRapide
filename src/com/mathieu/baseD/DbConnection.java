d73d66431e9d7ff46c292125097cfef03ac8a726
Mathieu Loyer <loyer.mathieu@courrier.uqam.qc.ca>
2016-10-22 12:10:41 -0400

    refactorisation classe DBconnect

--- src/com/mathieu/baseD/DbConnection.java
+++ src/com/mathieu/baseD/DbConnection.java
@@ -7,29 +7,31 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DbConnection {
+
     // class Vars
     String idUsagerAtrouver = ""; // id a obtenir
     Connection conn = null;
+    Statement stmt = null;
     // vars for test
     String utilisateur = "utilisateur"; // c'est la table de la DB to select from.
     String idUsager = ""; // remplacer par userObj.idUsager
     ResultSet rs = null; // objet qui contient le(s) resultat(s) ou NULL;
     String SQL = null;
-    
+
     /**
-     * constructeur
-     * String s n'est pas utilise pour le moment
+     * constructeur String s n'est pas utilise pour le moment
      */
-    public DbConnection(String usagerAtrouver, String SQL){  // Remplacer paremetre par ObjUtilisateur U selon besoins
+    public DbConnection(String usagerAtrouver, String SQL) {  // Remplacer paremetre par ObjUtilisateur U selon besoins
         this.SQL = SQL;
         //idUsagerAtrouver = usagerAtrouver; // will be replaced by primary key eventually
         Connection conn = makeConnection();
+        Statement stmt = makeStatement();
         //insertData( conn );
         //rs = getDataFromDb(conn);
         //closeConnection(conn);
     }
 
-    private Connection makeConnection(){
+    private Connection makeConnection() {
 
         String url = "jdbc:mysql://localhost:8889/services";
         String usr = "root";
@@ -42,8 +44,8 @@ public class DbConnection {
         }
         // once the driver is registered, make connection
         try {
-            conn =
-            DriverManager.getConnection(url, usr, psw);
+            conn
+                    = DriverManager.getConnection(url, usr, psw);
         } catch (SQLException ex) {
             // handle any errors
             System.out.println("SQLException: " + ex.getMessage());
@@ -53,60 +55,57 @@ public class DbConnection {
         System.out.println("Made Connection");
         return conn;
     }
-    
-    
-    private void insertData(Connection conn){
-        System.out.println("Start Update");
+
+    private Statement makeStatement() {
         Statement stmt = null;
-        ResultSet rs = null;
-        try{
+        try {
             stmt = conn.createStatement();
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             System.out.println("Error Getting Data: stmt");
         }
-        try{
-            System.out.println("SQL string is: " + SQL);
-           
-              stmt.executeUpdate(SQL);
-        } catch (SQLException ex){
-            
-            System.out.println(ex +  "    Error Getting Data: update");
-        }
-        System.out.println("End Update");
-    
+        return stmt;
     }
 
-    private ResultSet getDataFromDb(Connection conn){
-        System.out.println("Start get RS");
-        Statement stmt = null;
+    private void insertData(Connection conn) {
+        System.out.println("Start Update");
         ResultSet rs = null;
-        try{
-            stmt = conn.createStatement();
-        } catch (SQLException ex){
-            System.out.println("Error Getting Data: stmt");
+
+        try {
+            System.out.println("SQL string is: " + SQL);
+
+            stmt.executeUpdate(SQL);
+        } catch (SQLException ex) {
+
+            System.out.println(ex + "    Error Getting Data: update");
         }
-        try{
+        System.out.println("End Update");
+
+    }
+
+    private ResultSet getDataFromDb(Connection conn) {
+        System.out.println("Start get RS");
+        ResultSet rs = null;
+        try {
             System.out.println("SQL string is: " + SQL);
             //rs = stmt.executeQuery(SQL);
-              stmt.executeQuery(SQL);
-        } catch (SQLException ex){
+            stmt.executeQuery(SQL);
+        } catch (SQLException ex) {
             System.out.println("    Error Getting Data: rs");
         }
         System.out.println("End get RS");
         return rs;
     }
 
-    public void closeConnection(){
-        try{
+    public void closeConnection() {
+        try {
             conn.close();
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             System.out.println("Error Closing Connection");
         }
         System.out.println("");
     }
 
-    public ResultSet getResultSet(){
+    public ResultSet getResultSet() {
         return this.rs;
     }
 }
-
